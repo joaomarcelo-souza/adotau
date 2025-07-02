3c085afca5e075fa70a3f0c0ae86fde737961492
JoaoMarcelo Souza/LGESP DX Software Part - MAO <joaomarcelo.souza@lge.com>
2025-07-02 14:20:14 -0400

    feat: About component created

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-about',
+  imports: [Navbar],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class About {
+
+}
