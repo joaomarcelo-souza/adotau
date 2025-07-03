265ab48c23448c97cb43f910922644c259004bec
JoaoMarcelo Souza/LGESP DX Software Part - MAO <joaomarcelo.souza@lge.com>
2025-07-03 15:34:29 -0400

    feat: Register page added to project

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-register',
+  imports: [Navbar],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class Register {
+
+}
