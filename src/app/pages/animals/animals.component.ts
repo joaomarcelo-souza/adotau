f539e01893dcb221edb0eef51a7546c9ce4bbcee
JoaoMarcelo Souza/LGESP DX Software Part - MAO <joaomarcelo.souza@lge.com>
2025-07-03 15:30:37 -0400

    feat: Now the home page is the Animals page

---
+++ src/app/pages/animals/animals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-animals',
+  imports: [Navbar],
+  templateUrl: './animals.component.html',
+  styleUrl: './animals.component.scss'
+})
+export class Animals {
+
+}
