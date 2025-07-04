bc801dcf6ab6e9bf9f79d01cf4bb7dd05ae85b8d
DaviCarvalho <davicarvalho425@gmail.com>
2025-07-03 22:53:27 -0400

    feat: Register_animals component added in he project

---
+++ src/app/pages/register-animals/register-animals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-animals',
+  imports: [],
+  templateUrl: './register-animals.html',
+  styleUrl: './register-animals.scss'
+})
+export class RegisterAnimals {
+
+}
