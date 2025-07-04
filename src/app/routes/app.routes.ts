83c76c0439da9288a0bb958d3dc6da8297c0a746
DaviCarvalho <davicarvalho425@gmail.com>
2025-07-03 21:17:28 -0400

    feat: Login route added to routes

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Animals } from '../pages/animals/animals.component';
 import { Register } from '../pages/register/register.component';
+import { Login } from '../pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: Animals },
     { path: 'cadastrar', component: Register },
+    { path: 'login',component: Login }
 ];
