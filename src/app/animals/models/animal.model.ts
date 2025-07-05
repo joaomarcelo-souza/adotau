f2acee91f632ae35290546290d1d4553c23ce8b2
João Marcelo <marceloamaral201924@gmail.com>
2025-07-04 22:33:46 -0400

    feat: New animal model, with animal atributes

---
+++ src/app/animals/models/animal.model.ts
@@ -0,0 +1,13 @@
+export interface Animal {
+    id: number;
+    name: string;
+    species: string;
+    sex: string;
+    age: number;
+    size: string;
+    description: string;
+    city: string;
+    neighborhood: string;
+    photoUrl: string;
+    donorId: number;
+}
