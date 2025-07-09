e335fc77968db28906b9e27f06b290ed75bc46a1
João Marcelo <marceloamaral201924@gmail.com>
2025-07-08 20:26:38 -0400

    feat: Animal component created

---
+++ src/app/pages/animal-profile/animal-profile.component.ts
@@ -0,0 +1,26 @@
+import { Component, computed, inject } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { AbstractAnimalService } from '../../animals/services/abstract-animal.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-animal-profile',
+  imports: [Navbar],
+  templateUrl: './animal-profile.component.html',
+  styleUrl: './animal-profile.component.scss'
+})
+export class AnimalProfile {
+  private route = inject(ActivatedRoute);
+  private animalService = inject(AbstractAnimalService);
+
+  animalId = toSignal(this.route.params.pipe(
+    map(params => parseInt(params['id']))
+  ));
+
+  animal = computed(() => {
+    const id = this.animalId();
+    return id ? this.animalService.getAnimalById(id)() : undefined;
+  });
+}
