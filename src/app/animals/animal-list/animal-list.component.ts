de15f9663ebad98ed23ae8d62209046c7bcc7366
João Marcelo <marceloamaral201924@gmail.com>
2025-07-08 20:44:08 -0400

    feat: Updating some variables names and types

--- src/app/animals/animal-list/animal-list.component.ts
+++ src/app/animals/animal-list/animal-list.component.ts
@@ -1,17 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { AbstractAnimalService } from '../services/abstract-animal.service';
 import { AnimalCard } from "../animal-card/animal-card.component";
+import { Animal } from '../models/animal.model';
 
 @Component({
   selector: 'app-animal-list',
+  standalone: true,
   imports: [AnimalCard],
   templateUrl: './animal-list.component.html',
   styleUrl: './animal-list.component.scss'
 })
 export class AnimalList {
-  private service = inject(AbstractAnimalService)
+  private animalService = inject(AbstractAnimalService);
 
-  animals = this.service.animals;
+  animals = this.animalService.animals;
 
-  trackById = (_:number, item:any) => item.id 
+  // Corrigindo a tipagem do trackById
+  trackById = (index: number, animal: Animal) => animal.id;
 }
