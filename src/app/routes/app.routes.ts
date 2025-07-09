22d6078e9d79023c99a9ab528056fe611b58b662
João Marcelo <marceloamaral201924@gmail.com>
2025-07-08 20:49:49 -0400

    feat: New route added that shows the animal profile based on id

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Animals } from '../pages/animals/animals.component';
 import { Register } from '../pages/register/register.component';
 import { Login } from '../pages/login/login.component';
+import { AnimalProfile } from '../pages/animal-profile/animal-profile.component';
 
 export const routes: Routes = [
     { path: '', component: Animals },
     { path: 'cadastrar', component: Register },
-    { path: 'login',component: Login }
+    { path: 'login',component: Login },
+    { path: 'animais/:id', component: AnimalProfile }
 ];
