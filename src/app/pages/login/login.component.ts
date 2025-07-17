f915862006107138efd4f1b25407835bdb8124b5
Davi <davicarvalho425@gmail.com>
2025-07-17 16:17:18 -0400

    feat: Some import from angular material were done. And a consifuration of input password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
+import { MatCard } from "@angular/material/card";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
-  imports: [Navbar],
+  imports: [Navbar, MatCard, MatInputModule,  MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class Login {
+showPassword: any;
+  
 
 }
