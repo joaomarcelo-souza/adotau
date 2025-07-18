1f7c2a8ecfd6532b714b33814d3e5845bacc2221
João Souza <marceloamaral201924@gmail.com>
2025-07-18 07:32:49 -0400

    Revert "feat: initial page decorations"

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { Animals } from '../pages/animals/animals.component';
-import { Register } from '../pages/register/register.component';
-import { Login } from '../pages/login/login.component';
-import { AnimalProfile } from '../pages/animal-profile/animal-profile.component';
-import { UserProfile } from '../pages/user-profile/user-profile.component';
+import { Home } from '../pages/home/home.component';
+import { About } from '../pages/about/about.component';
 
 export const routes: Routes = [
-    { path: '', component: Animals },
-    { path: 'cadastrar', component: Register },
-    { path: 'login',component: Login },
-    { path: 'animais/:id', component: AnimalProfile }, 
-    { path: 'user-profile', component: UserProfile }
+    { path: '', component: Home },
+    { path: 'about', component: About },
 ];
