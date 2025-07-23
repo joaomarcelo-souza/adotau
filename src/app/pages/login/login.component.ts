6becdb36fcf5e654a723fbf648f3b700709890e3
João Marcelo <marceloamaral201924@gmail.com>
2025-07-23 11:09:16 -0400

    feat: now the login form is a separated component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
-import { MatCard } from "@angular/material/card";
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { UserForm } from '../../users/user-form/user-form.component';
 
 @Component({
   selector: 'app-login',
-  imports: [Navbar, MatCard, MatInputModule,  MatFormFieldModule],
+  standalone: true,
+  imports: [Navbar, UserForm],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class Login {
-showPassword: any;
-  
 
 }
