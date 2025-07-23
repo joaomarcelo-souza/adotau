f5352bcd4caaf753fefb2fd155bb66803db8a7cb
João Marcelo <marceloamaral201924@gmail.com>
2025-07-23 08:11:26 -0400

    feat: New functions added to the abstract user service

--- src/app/users/service/abstract-user.service.ts
+++ src/app/users/service/abstract-user.service.ts
@@ -1,7 +1,13 @@
 import { Signal } from "@angular/core";
 import { User } from "../models/user.model";
+import { Observable } from "rxjs";
 
 export abstract class AbstractUserService {
-  abstract user: Signal<User[]>;
-  abstract getUserById(id: number): Signal<User | undefined>;
+  abstract users: Signal<User[]>;
+  abstract refresh(): void;
+  abstract add(user: Omit<User, 'id' | 'createdDate'>): Observable<OperationResult>;
+  abstract remove(id: number): Observable<OperationResult>;
+  abstract update(user: User): Observable<OperationResult>;
+  abstract search_by_id(id: number): Observable<OperationResult>;
+  abstract login(query: any): Observable<OperationResult>;
 }
