1ed7fad1f35fee66e3bb082b0398a4cab0b8efa5
João Marcelo <marceloamaral201924@gmail.com>
2025-07-23 23:53:00 -0400

    feat: app routes updated

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -5,11 +5,14 @@ import { AnimalProfile } from '../pages/animal-profile/animal-profile.component'
 import { UserProfile } from '../pages/user-profile/user-profile.component';
 import { Login } from '../pages/login/login.component';
 import { authGuard } from '../services/auth/auth.guard';
+import { donorGuard } from '../services/auth/donor.guard';
 
 export const routes: Routes = [
     { path: '', component: Animals },
     { path: 'cadastrar', component: Register },
-    { path: 'login', component: Login},
-    { path: 'animais/:id', component: AnimalProfile }, 
-    { path: 'profile', component: UserProfile, canActivate: [authGuard] }
+    { path: 'login', component: Login },
+    { path: 'animais/:id', component: AnimalProfile },
+    { path: 'profile', component: UserProfile, canActivate: [authGuard] },
+    { path: 'cadastrar-animal', component: Register, canActivate: [authGuard, donorGuard] },
+    { path: 'alterar-animal/:id', component: Register, canActivate: [authGuard, donorGuard] }
 ];
