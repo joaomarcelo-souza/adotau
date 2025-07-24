6c9c92527eb4257b90d70d8c8e3a59caf9b8d4fa
JoaoMarcelo Souza/LGESP DX Software Part - MAO <joaomarcelo.souza@lge.com>
2025-07-24 16:16:53 -0400

    feat: animal card updated

--- src/app/animals/animal-card/animal-card.component.ts
+++ src/app/animals/animal-card/animal-card.component.ts
@@ -4,11 +4,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-animal-card',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, MatMenuModule, RouterModule],
+  imports: [MatCardModule, MatIconModule, MatMenuModule, RouterModule, MatButtonModule],
   templateUrl: './animal-card.component.html',
   styleUrl: './animal-card.component.scss'
 })
