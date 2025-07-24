6d09f94708b3965e001a2c196afae9c7a1d69270
João Marcelo <marceloamaral201924@gmail.com>
2025-07-23 23:53:51 -0400

    feat: register component now using animal-form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
+import { AnimalForm } from '../../animals/animal-form/animal-form.component';
 
 @Component({
   selector: 'app-register',
-  imports: [Navbar],
+  imports: [Navbar, AnimalForm],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
