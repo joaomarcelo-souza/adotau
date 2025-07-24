3adb9744b0c04bf4156bde4cb6a74e089b852d11
João Marcelo <marceloamaral201924@gmail.com>
2025-07-24 09:10:32 -0400

    feat: new component animal user card, to animals on user profiles

---
+++ src/app/animals/animal-user-card/animal-user-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Animal } from '../models/animal.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-animal-user-card',
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, MatMenuModule, RouterModule],
+  templateUrl: './animal-user-card.component.html',
+  styleUrl: './animal-user-card.component.scss'
+})
+export class AnimalUserCard {
+  @Input() animal!: Animal;
+}
