0b1a96675cae1c9572f1fbb43b73c373155ef375
João Marcelo <marceloamaral201924@gmail.com>
2025-07-23 23:50:17 -0400

    feat: authenticator guard created

--- src/app/services/auth/auth.guard.ts
+++ src/app/services/auth/auth.guard.ts
@@ -1,15 +1,18 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { FeedbackService } from '../feedback/feedback.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const feedbackService = inject(FeedbackService);
 
   if (authService.isLoggedIn()) {
     return true;
   } else {
-    router.navigate(['/login']);
+    feedbackService.error('Você precisa estar logado para acessar esta página.');
+    router.navigate(['/']);
     return false;
   }
 };
