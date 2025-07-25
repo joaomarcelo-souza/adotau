16b9647760b8b20773e1aaa675978e48a07f5b8a
JoaoMarcelo Souza/LGESP DX Software Part - MAO <joaomarcelo.souza@lge.com>
2025-07-25 16:16:35 -0400

    feat: New lists added one for cats and one for dogs

---
+++ src/app/animals/animal-lists/cat-list/cat-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, computed, inject } from '@angular/core';
+import { AbstractAnimalService } from '../../services/abstract-animal.service';
+import { Animal } from '../../models/animal.model';
+import { AnimalCard } from '../../animal-card/animal-card.component';
+
+@Component({
+  selector: 'app-cat-list',
+  imports: [AnimalCard],
+  templateUrl: './cat-list.component.html',
+  styleUrl: './cat-list.component.scss'
+})
+export class CatList {
+  private animalService = inject(AbstractAnimalService);
+  
+  animals = computed(() => 
+    this.animalService.animals().filter((animal: Animal) => animal.species === 'Gato')
+  );
+
+  trackById = (index: number, animal: Animal) => animal.id;
+}
