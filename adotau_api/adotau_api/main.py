194f2f570edecffb4584d834125650c303e58679
João Marcelo <marceloamaral201924@gmail.com>
2025-08-01 09:54:41 -0400

    feat: Creating the database and including base router on main

--- adotau_api/adotau_api/main.py
+++ adotau_api/adotau_api/main.py
@@ -1,6 +1,12 @@
 """Main file that creates the API, include the routes and create the database"""
 
 from fastapi import FastAPI
+from adotau_api.api.v1.routes import router
+from adotau_api.db.database_config import Base, engine
 
 
 app = FastAPI(title="Adotau")
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(router=router)
