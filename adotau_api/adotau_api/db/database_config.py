e356d2233ebd8a429628521a7f08a57b292bbd21
João Marcelo <marceloamaral201924@gmail.com>
2025-08-01 09:55:04 -0400

    fix: Fixing the database url

--- adotau_api/adotau_api/db/database_config.py
+++ adotau_api/adotau_api/db/database_config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 
-DATABASE_URL = "sqlit:///./adotau.db"
+DATABASE_URL = "sqlite:///./adotau.db"
 
 engine = create_engine(
     DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
