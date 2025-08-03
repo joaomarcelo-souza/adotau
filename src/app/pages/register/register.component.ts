b372d652403511117e1b80412c956ef3ee6c9122
DaviCarvalho <davicarvalho425@gmail.com>
2025-08-03 17:20:05 -0400

    feat: import the UserForm to the register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
 import { AnimalForm } from '../../animals/animal-form/animal-form.component';
+import { UserForm } from '../../users/user-form/user-form.component';
 
 @Component({
   selector: 'app-register',
-  imports: [Navbar],
+  imports: [UserForm],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
