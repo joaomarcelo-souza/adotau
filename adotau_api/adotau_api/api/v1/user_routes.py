cbca5a79eb423cef99748843a4998a0fd11bcb04
João Marcelo <marceloamaral201924@gmail.com>
2025-08-09 17:45:34 -0400

    feat: decoding token user to athentication

--- adotau_api/adotau_api/api/v1/user_routes.py
+++ adotau_api/adotau_api/api/v1/user_routes.py
@@ -10,13 +10,14 @@ from adotau_api.services.user_service import (
     update_user,
     delete_user,
 )
+from adotau_api.core.auth import decodeing_token_user
 from adotau_api.db.database_config import get_db, Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 router = APIRouter(prefix="/v1/user", tags=["User"])
 
 
-@router.post("/", response_model=UserRead, status_code=status.HTTP_200_OK)
+@router.post("/", response_model=UserRead, status_code=status.HTTP_201_CREATED)
 def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     """Route that creates a new User"""
     return create_user(db, user)
@@ -25,12 +26,23 @@ def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
 @router.get("/list", response_model=list[UserRead], status_code=status.HTTP_200_OK)
 def list_users(db: Session = Depends(get_db)):
     """Route that lists all Users"""
+
+    if not decodeing_token_user(token):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Erro ao validar token"
+        )
+
     return get_all_users(db)
 
 
 @router.get("/{user_id}", response_model=UserRead, status_code=status.HTTP_200_OK)
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     """Route that gets a User by his id"""
+
+    if not decodeing_token_user(token):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Erro ao validar token"
+        )
 
     user = get_user(db, user_id)
 
@@ -46,6 +58,11 @@ def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
 def update_user_by_id(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
     """Route that updates a User by his id"""
 
+    if not decodeing_token_user(token):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Erro ao validar token"
+        )
+
     updated = update_user(db, user_id, user)
 
     if not updated:
@@ -59,6 +76,11 @@ def update_user_by_id(user_id: int, user: UserUpdate, db: Session = Depends(get_
 @router.delete("/{user_id}", status_code=status.HTTP_200_OK)
 def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
     """Route that deletes a User"""
+
+    if not decodeing_token_user(token):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Erro ao validar token"
+        )
 
     deleted = delete_user(db, user_id)
 
