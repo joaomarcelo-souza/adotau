25b7d6b8869a80789761362b313b8954057fc53b
João Marcelo <marceloamaral201924@gmail.com>
2025-08-09 17:39:12 -0400

    feat: base router added to the app

--- adotau_api/adotau_api/main.py
+++ adotau_api/adotau_api/main.py
@@ -1,6 +1,7 @@
 """Main file that creates the API, include the routes and create the database"""
 
 from fastapi import FastAPI
+from adotau_api.api.v1.routes import router
 from adotau_api.api.v1.user_routes import router as user_router
 from adotau_api.api.v1.animal_routes import router as animal_router
 
@@ -11,5 +12,6 @@ app = FastAPI(title="Adotau")
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(router=router)
 app.include_router(router=user_router)
 app.include_router(router=animal_router)
