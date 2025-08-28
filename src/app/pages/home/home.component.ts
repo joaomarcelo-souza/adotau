f85a822c9ed12fc1b9155ead5840f940319cb429
João Marcelo <marceloamaral201924@gmail.com>
2025-08-28 07:58:56 -0400

    feat: new section in home, featured-section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
+import { FeaturedAnimals } from '../../animals/animal-lists/featured-animals/featured-animals.component';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, FeaturedAnimals],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
