b009ec87841e6f0023e1144657596fe052dac1c0
João Marcelo <marceloamaral201924@gmail.com>
2025-08-28 08:34:43 -0400

    feat: using mat icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
 import { FeaturedAnimals } from '../../animals/animal-lists/featured-animals/featured-animals.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, FeaturedAnimals],
+  imports: [Navbar, FeaturedAnimals, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
