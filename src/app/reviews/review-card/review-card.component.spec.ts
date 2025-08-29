b7eef05b6442a7f7eb43a176f2206ff2ae815532
João Marcelo <marceloamaral201924@gmail.com>
2025-08-29 09:10:11 -0400

    feat: new review card and list components added

---
+++ src/app/reviews/review-card/review-card.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReviewCard } from './review-card.component';
+
+describe('ReviewCard', () => {
+  let component: ReviewCard;
+  let fixture: ComponentFixture<ReviewCard>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReviewCard],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ReviewCard);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
