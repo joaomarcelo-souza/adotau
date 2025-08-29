3bc7e9f96383d54e7e6b0086983f1484387c8d57
João Marcelo <marceloamaral201924@gmail.com>
2025-08-29 09:11:31 -0400

    feat: new imports on the ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
 import { FeaturedAnimals } from '../../animals/animal-lists/featured-animals/featured-animals.component';
 import { MatIconModule } from '@angular/material/icon';
+import { ReviewList } from '../../reviews/review-list/review-list.component';
+import { Footer } from '../../components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, FeaturedAnimals, MatIconModule],
+  imports: [
+    Navbar,
+    FeaturedAnimals,
+    MatIconModule,
+    ReviewList,
+    Footer,
+    RouterModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
