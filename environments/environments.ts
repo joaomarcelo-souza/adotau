04242072dbab5c5a8e509fed83e39c6f4b6cd692
João Marcelo <marceloamaral201924@gmail.com>
2025-09-05 09:46:16 -0400

    fix: environments updated

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    useMockService: true,
-    apiUrl: "http://localhost:8000/api",
-}
+  production: false,
+  useMockService: false,
+  apiUrl: 'http://127.0.0.1:8000',
+};
