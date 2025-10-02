44d198e42096b984348742e9fe220f4cf8c707f2
João Marcelo <marceloamaral201924@gmail.com>
2025-10-02 08:14:06 -0400

    feat: now using dotenv and psycopg to connect with postgres

--- backend/adotau_api/db/database_config.py
+++ backend/adotau_api/db/database_config.py
@@ -1,10 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./adotau.db"
+# Carrega variáveis do arquivo .env
+load_dotenv()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = (
+    f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
 
 engine = create_engine(
-    DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    echo=True,
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
@@ -12,15 +27,14 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False
 
 def get_db():
     """Get the database session"""
-
     db: Session = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
 
 
 class Base(DeclarativeBase):
-    """Class that extends the DeclarativeBase from sqlalchemy"""
+    """Declarative Base for models"""
+
+    pass
