3060749a01e227af27156caaf73a6a08032c9391
João Marcelo <marceloamaral201924@gmail.com>
2025-10-06 08:50:35 -0400

    feat: review routes added on the main file

--- backend/adotau_api/main.py
+++ backend/adotau_api/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from adotau_api.api.v1.routes import router
 from adotau_api.api.v1.user_routes import router as user_router
 from adotau_api.api.v1.animal_routes import router as animal_router
+from adotau_api.api.v1.review_routes import router as review_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
@@ -30,3 +31,4 @@ Base.metadata.create_all(bind=engine)
 app.include_router(router=router)
 app.include_router(router=user_router)
 app.include_router(router=animal_router)
+app.include_router(router=review_router)
