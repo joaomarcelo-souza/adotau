e5bda5d6d4247179daaf6100a5ec3e93eb3828b3
João Marcelo <marceloamaral201924@gmail.com>
2025-10-13 08:49:06 -0400

    fix: review list commented for now

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,20 +2,12 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar.component';
 import { FeaturedAnimals } from '../../animals/animal-lists/featured-animals/featured-animals.component';
 import { MatIconModule } from '@angular/material/icon';
-import { ReviewList } from '../../reviews/review-list/review-list.component';
 import { Footer } from '../../components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    Navbar,
-    FeaturedAnimals,
-    MatIconModule,
-    ReviewList,
-    Footer,
-    RouterModule,
-  ],
+  imports: [Navbar, FeaturedAnimals, MatIconModule, Footer, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
