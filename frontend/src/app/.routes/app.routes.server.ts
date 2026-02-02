527c9742730f1debf8fe31af3cb27215ff9a0dc1
João Souza <marceloamaral201924@gmail.com>
2026-02-02 08:12:57 -0400

    fix: render mode altered for dinamic routes

--- frontend/src/app/.routes/app.routes.server.ts
+++ frontend/src/app/.routes/app.routes.server.ts
@@ -2,6 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'animais/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'profile/animais/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'profile/alterar/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'profile/alterar-user/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
