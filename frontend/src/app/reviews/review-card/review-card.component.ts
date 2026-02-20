01017876fb8ac60ee46bd361f74d18af09cf7c10
João Souza <marceloamaral201924@gmail.com>
2026-02-20 15:04:12 -0400

    remove: reviews feature removed

--- frontend/src/app/reviews/review-card/review-card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-review-card',
-  imports: [],
-  templateUrl: './review-card.component.html',
-  styleUrl: './review-card.component.scss',
-})
-export class ReviewCard {}
